fcb5cb1f3a4747501804b0031b428a04021977a7
Agustin Lorenzatti <lorenzatiro@gmail.com>
2024-01-15 10:33:11 -0300

    exhbs

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ const PORT = 8080;
 
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
 const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(".src/public"));
 
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+app.use("/", viewsRouter);
 app.use("/api/products", productsRouter);
-
 app.use("/api/carts", cartsRouter);
 
 app.listen(PORT, () => {
