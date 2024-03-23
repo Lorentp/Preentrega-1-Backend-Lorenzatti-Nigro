87dc119d1b87ea5c5a03e30f2512d6554fcf70d0
Agustin Lorenzatti <lorenzatiro@gmail.com>
2024-03-23 19:46:02 -0300

    desafio listo

--- src/database.js
+++ src/database.js
@@ -1,12 +1,31 @@
 const mongoose = require("mongoose");
-mongoose
-  .connect(
-    "mongodb+srv://loren:jalnlorenza@coderbackend.zwmjgoq.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=coderbackend"
-  )
+const configObject = require("./config/config");
+const { mongo_url } = configObject;
+/*mongoose
+  .connect(mongo_url)
   .then(() => console.log("Conexion a la base de datos exitosa"))
   .catch((error) =>
     console.log(
       "Ha ocurrido un error a la hora de conectar a la base de datos",
       error
     )
-  );
+  );*/
+
+class DataBase {
+  static #intance;
+
+  constructor() {
+    mongoose.connect(mongo_url);
+  }
+  static getIntance() {
+    if (this.#intance) {
+      console.log("Conexion ya existente");
+      return this.#intance;
+    }
+    tihs.#intance = new DataBase();
+    console.log("Conexion exitosa");
+    return this.#intance;
+  }
+}
+
+module.exports = DataBase.getIntance();
